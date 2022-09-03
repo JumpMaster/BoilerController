0ffbcd4a32bd303598048de2b2edd387eeaff994
JumpMaster <kevin@cooper.uk>
2022-09-03 09:59:22 +0100

    Initial commit

---
+++ BoilerControllerESP32.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef BOILERCONTROLLER_H
+#define BOILERCONTROLLER_H
+
+    typedef enum {
+        DEVICE_DISCONNECTED = 0,
+        DEVICE_WIFI_CONNECTED = 1,
+        DEVICE_MQTT_CONNECTED = 2
+    } DeviceConnectionState;
+
+    typedef enum {
+        BOILER_NORMAL = 0,
+        BOILER_SAFTEY_MODE_PENDING = 1,
+        BOILER_SAFTEY_MODE = 2
+    } BoilerMode;
+
+#endif // BOILERCONTROLLER_H
